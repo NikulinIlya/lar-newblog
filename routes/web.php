a422aa0e03319022201d4db6c94d464c67aecec8
NikulinIlya <ilia.nikulin@gmail.com>
2019-04-05 12:00:08 +0300

    feat(routes): make routes for admin blog categories controller

--- routes/web.php
+++ routes/web.php
@@ -15,10 +15,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
 Route::group(['namespace' => 'Blog', 'prefix' => 'blog'], function () {
     Route::resource('posts', 'PostController')->names('blog.posts');
 });
 
-Auth::routes();
+$groupData = [
+    'namespace' => 'Blog\Admin',
+    'prefix' => 'admin/blog'
+];
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::group($groupData, function () {
+    $methods = ['index', 'edit', 'update', 'create', 'store'];
+    Route::resource('categories', 'CategoryController')
+        ->only($methods)
+        ->names('blog.admin.categories');
+});
+
