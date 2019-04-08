9efe24b1b59b5830430a0aaa9003c041ca640215
NikulinIlya <ilia.nikulin@gmail.com>
2019-04-08 19:38:39 +0300

    fix(item_edit_add_col.blade): change @php blade directive

--- resources/views/blog/admin/category/includes/item_edit_add_col.blade.php
+++ resources/views/blog/admin/category/includes/item_edit_add_col.blade.php
@@ -1,4 +1,4 @@
-@php(/** @var \App\Models\BlogCategory $item */)
+@php /** @var \App\Models\BlogCategory $item */ @endphp
 <div class="row justify-content-center">
     <div class="col-md-12">
         <div class="card">
