af73f8a493caf6e98357ee5ad02ce905820b4feb
NikulinIlya <ilia.nikulin@gmail.com>
2019-04-08 22:43:48 +0300

    feat(item_edit_main_col): add using old('description') helper function

--- resources/views/blog/admin/categories/includes/item_edit_main_col.blade.php
+++ resources/views/blog/admin/categories/includes/item_edit_main_col.blade.php
@@ -46,8 +46,8 @@
 
                         <div class="form-group">
                             <label for="description">Описание</label>
-                            <textarea name="description" id="description" rows="10" class="form-control">
-                                {{ $item->description }}
+                            <textarea name="description" id="description" rows="3" class="form-control">
+                                {{ old('description', $item->description) }}
                             </textarea>
                         </div>
                     </div>
