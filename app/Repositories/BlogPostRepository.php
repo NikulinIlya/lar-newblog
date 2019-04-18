e6cee624aa135198fdac6e6181d9da292be24f04
NikulinIlya <ilia.nikulin@gmail.com>
2019-04-18 16:41:06 +0300

    feat(Repositories): make BlogPostRepository

---
+++ app/Repositories/BlogPostRepository.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\BlogCategory as Model;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Database\Eloquent\Collection;
+
+/**
+ * Class BlogPostRepository
+ *
+ * @package App\Repositories
+ */
+class BlogPostRepository extends CoreRepository
+{
+    /**
+     * @return string
+     */
+    protected function getModelClass()
+    {
+        return Model::class;
+    }
+
+}
