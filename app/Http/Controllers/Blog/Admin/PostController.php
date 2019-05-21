b521c8da2969d72609842b0ab5f496e49444da81
NikulinIlya <ilia.nikulin@gmail.com>
2019-05-21 16:20:03 +0300

    feat(PostController): change edit method

--- app/Http/Controllers/Blog/Admin/PostController.php
+++ app/Http/Controllers/Blog/Admin/PostController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Blog\Admin;
 
+use App\Repositories\BlogCategoryRepository;
 use App\Repositories\BlogPostRepository;
 use Illuminate\Http\Request;
 
@@ -17,10 +18,17 @@ class PostController extends BaseController
      */
     private $blogPostRepository;
 
+    /**
+     * @var BlogCategoryRepository
+     */
+    private $blogCategoryRepository;
+
     public function __construct()
     {
         parent::__construct();
+
         $this->blogPostRepository = app(BlogPostRepository::class);
+        $this->blogCategoryRepository = app(BlogCategoryRepository::class);
     }
 
     /**
@@ -75,7 +83,15 @@ class PostController extends BaseController
      */
     public function edit($id)
     {
-        //
+        $item = $this->blogPostRepository->getEdit($id);
+        if (empty($item)) {
+            abort(404);
+        }
+
+        $categoryList = $this->blogCategoryRepository->getForComboBox();
+
+        return view('blog.admin.posts.edit',
+            compact('item', 'categoryList'));
     }
 
     /**
